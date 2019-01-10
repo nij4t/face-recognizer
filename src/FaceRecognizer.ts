e29ef995a254d75eed8d76d3cd6d51112583feea
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2019-01-10 12:40:14 +0400

    Metamorphosis phase

---
+++ src/FaceRecognizer.ts
@@ -0,0 +1,32 @@
+import * as faceapi from "face-api.js";
+import { Canvas, Image, ImageData } from "canvas";
+import { readdirSync, writeFileSync, readFileSync } from "fs";
+
+require("@tensorflow/tfjs-node");
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+
+const MODEL_URI = "./models/";
+
+export default class FaceRecognizer {
+  private static instance: FaceRecognizer;
+  private cb: Function;
+
+  private constructor() {
+    Promise.all([
+      faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URI),
+      faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URI),
+      faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URI)
+    ]).then(() => this.cb())
+  }
+
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new FaceRecognizer();
+    }
+    return this.instance;
+  }
+
+  onModelsReady(cb) {
+    this.cb = cb;
+  }
+}
---
+++ tests/FaceRecognizer.test.ts
@@ -0,0 +1,17 @@
+import { assert } from "chai";
+import FaceRecognizer from "../src/FaceRecognizer";
+
+suite("FaceRecognizer Test Suite", () => {
+  let fr = FaceRecognizer.getInstance();
+
+  test("Create FaceRecognizer instance", done => {
+    assert.isNotNull(fr);
+    assert.instanceOf(fr, FaceRecognizer)
+    done();
+  });
+
+  test("Load models successful", done => {
+    fr.onModelsReady(() => done())
+  });
+
+});

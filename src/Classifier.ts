b8e513cd31fc0e157fe3f3115271db0a44bfd6c9
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2019-02-05 16:58:29 +0400

    Fix bugs

--- src/Classifier.ts
+++ src/Classifier.ts
@@ -1,7 +1,7 @@
 import { ISerializable } from "./ISerializable";
 import { IDeserializable } from "./IDeserializable";
 import { ImageSet } from "./ImageSet";
-import { readdirSync } from "fs";
+import { readdirSync, statSync } from "fs";
 import { resolve } from "path";
 
 export class Classifier implements ISerializable, IDeserializable<ImageSet[]> {
@@ -12,22 +12,23 @@ export class Classifier implements ISerializable, IDeserializable<ImageSet[]> {
   deserialize(path: string): void {
     throw new Error("Method not implemented.");
   }
-  
+
   public getImageSets(dir: string): ImageSet[] {
     return this.getClassNames(dir).map(className => {
-      return ({
+      return {
         name: className,
-        images: this.getImages(resolve(dir, className)).map(img => resolve(dir, className, img))
-      } as ImageSet);
+        images: this.getImages(resolve(dir, className)).map(img =>
+          resolve(dir, className, img)
+        )
+      } as ImageSet;
     });
   }
 
   private getClassNames(dir: string): string[] {
-    return readdirSync(resolve(dir));
+    return readdirSync(resolve(dir)).filter(entity => statSync(resolve(dir, entity)).isDirectory())
   }
 
   private getImages(dir: string): string[] {
     return readdirSync(dir);
   }
-
 }
